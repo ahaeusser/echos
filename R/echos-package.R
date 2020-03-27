da56618437a304bf303a4ee21e4a2ac86c416b6a
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-03-27 17:45:38 +0100

    Adjusted package dependencies

--- R/echos-package.R
+++ R/echos-package.R
@@ -1,19 +1,18 @@
 
-#' @import Rcpp
-#' @import RcppArmadillo
-#' @import dplyr
-#' @import tsibble
-#' @import ggplot2
-#' @import fabletools
 #' @importFrom purrr map_dfr invoke
 #' @importFrom tidyr gather spread
 #' @importFrom matrixStats colCumsums colDiffs colMins colMaxs
 #' @importFrom Matrix rsparsematrix
 #' @importFrom bestNormalize orderNorm
+#' @import Rcpp
+#' @import RcppArmadillo
+#' @import dplyr
+#' @import tsibble
+#' @import fabletools
 #' @useDynLib echos, .registration = TRUE
-#' @name echos
-NULL
 
 .onUnload <- function (libpath) {
   library.dynam.unload("echos", libpath)
 }
+
+NULL
