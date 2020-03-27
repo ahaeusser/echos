33cb051ed94801fb58ced2def851bc8eca23f9d2
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-03-27 17:42:26 +0100

    Adjusted documentation and class

--- R/ESN.R
+++ R/ESN.R
@@ -1,4 +1,5 @@
-#' @title Train ESN.
+
+#' @title Train an Echo State Network (ESN).
 #' 
 #' @description Train an Echo State Network (ESN).
 #'
@@ -6,7 +7,7 @@
 #' @param specials Specials as list defined in \code{specials_esn}.
 #' @param ... Further arguments passed to \code{echos::auto_esn(...)}.
 #'
-#' @return An object of class \code{fbl_esn}.
+#' @return An object of class \code{ESN}.
 
 train <- function(.data, specials, ...){
   
@@ -24,7 +25,6 @@ train <- function(.data, specials, ...){
   # Train model
   mdl <- echos::auto_esn(data = model_data, ...)
   
-  
   # Extract length of actual values and fitted values
   fitted <- mdl$fitted$.value
   resid <- mdl$resid$.value
@@ -32,19 +32,12 @@ train <- function(.data, specials, ...){
   n_total <- nrow(model_data)
   n_fitted <- length(fitted)
   
-  # alpha <- round(mdl$method$pars$alpha, 2)
-  # rho <- round(mdl$method$pars$rho, 2)
-  # lambda <- round(mdl$method$pars$lambda, 2)
-  # density <- round(mdl$method$pars$density, 2)
-  # pars <- c(alpha, rho, lambda, density)
-  
-  pars <- round(unlist(mdl$method$pars), 2)
-  
   # Fill NAs in front of fitted values (adjust to equal length of actual values) 
   fitted <- c(rep(NA_real_, n_total - n_fitted), fitted)
   resid <- c(rep(NA_real_, n_total - n_fitted), resid)
   
-  
+  # Model specification
+  model_spec <- mdl$method$model_spec
   
   # Return model
   structure(
@@ -53,21 +46,21 @@ train <- function(.data, specials, ...){
       est = list(
         .fitted = fitted,
         .resid = resid),
-      spec = pars),
-    class = "fbl_esn")
+      spec = model_spec),
+    class = "ESN")
 }
-
-
 
 
 specials_esn <- new_specials()
 
-#' @title Automatic training of ESNs
+#' @title Automatic training of ESNs.
+#' 
+#' @description Automatic training of ESNs.
 #'
 #' @param formula Model specification (see "Specials" section, currently not in use...)
 #' @param ... Further arguments passed to \code{echos::auto_esn(...)}.
 #'
-#' @return esn_model An object of class \code{fbl_esn}.
+#' @return esn_model An object of class \code{ESN}.
 #' @export
 
 ESN <- function(formula, ...){
@@ -83,45 +76,12 @@ ESN <- function(formula, ...){
 }
 
 
-#' @title Forecast a ELM.
-#'
-#' @description Forecast a Extreme Learning Machine (ELM).
-#'
-#' @param object The time series model used to produce the forecasts.
-#' @param new_data A tsibble containing future information used to forecast.
-#' @param specials Specials are currently not in use for MLPs.
-#' @param times The number of sample paths to use in estimating the forecast distribution when \code{bootstrap = TRUE}.
-#' @param ... Additional arguments for forecast model method.
-#'
-#' @return An object of class "fable".
-#' @export
-
-forecast.fbl_esn <- function(object,
-                             new_data,
-                             specials = NULL,
-                             times = 1000,
-                             ...){
-  # Extract model
-  mdl <- object$model
-  # Forecast model
-  fcst <- forecast_esn(mdl, h = nrow(new_data))
-
-  # Extract point forecast and simulations
-  mean <- fcst$mean
-  sim <- fcst$all.mean
 
 
-  # Return forecasts
-  construct_fc(
-    point = mean,
-    sd = rowSds(sim),
-    dist = dist_normal(mean = mean, sd = rowSds(sim)))
-}
 
-
-#' @title Extract fitted values from a ELM.
+#' @title Extract fitted values from a trained ESN.
 #' 
-#' @description Extract fitted values from a ELM.
+#' @description Extract fitted values from a trained ESN.
 #'
 #' @param object The time series model used to produce the forecast.
 #' @param ... Additional arguments passed to ...
@@ -129,14 +89,14 @@ forecast.fbl_esn <- function(object,
 #' @return
 #' @export
 
-fitted.fbl_esn <- function(object, ...){
+fitted.ESN <- function(object, ...){
   object$est[[".fitted"]]
 }
 
 
-#' @title Extract residuals from a MLP.
+#' @title Extract residuals from a trained ESN.
 #' 
-#' @description Extract residuals from a MLP.
+#' @description Extract residuals from a trained ESN.
 #'
 #' @param object The time series model used to produce the forecast.
 #' @param ... Additional arguments passed to ...
@@ -144,19 +104,20 @@ fitted.fbl_esn <- function(object, ...){
 #' @return
 #' @export
 
-residuals.fbl_esn <- function(object, ...){
+residuals.ESN <- function(object, ...){
   object$est[[".resid"]]
 }
 
-#' @title Provide a succinct summary of the MLP.
+
+#' @title Provide a succinct summary of a trained ESN.
 #' 
-#' @description Provide a succinct summary of the MLP.
+#' @description Provide a succinct summary of a trained ESN.
 #'
-#' @param object The MLP to summarize.
+#' @param object The ESN to summarize.
 #'
 #' @return
 #' @export
 
-model_sum.fbl_esn <- function(x){
-  paste("ESN(", "{", x$spec[1], ",", x$spec[2], ",", x$spec[3], "}", ")", sep = "")
+model_sum.ESN <- function(x){
+  x$spec
 }
