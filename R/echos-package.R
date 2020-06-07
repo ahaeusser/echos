024b1b1da827157e6fc4681a46612f4fb6e74b63
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-06-07 17:21:38 +0200

    Adjusted imports and exports

--- R/echos-package.R
+++ R/echos-package.R
@@ -3,7 +3,8 @@
 #' @importFrom tidyr gather spread
 #' @importFrom matrixStats colCumsums colDiffs colMins colMaxs rowSds
 #' @importFrom Matrix rsparsematrix
-#' @importFrom feasts unitroot_ndiffs unitroot_kpss
+#' @importFrom feasts unitroot_nsdiffs feat_stl unitroot_ndiffs unitroot_kpss
+#' @importFrom rlang is_empty .data
 #' @import Rcpp
 #' @import RcppArmadillo
 #' @import tsibble
