c8c6ebb5078d9257a362ec29fd6e9ff674c5c912
Alexander Haeusser <alexander-haeusser@gmx.de>
2020-09-25 13:08:41 +0200

    Removed package FitAR, removed feasts::unitroot_nsdiffs()

--- R/echos-package.R
+++ R/echos-package.R
@@ -4,11 +4,10 @@
 #' @importFrom readr parse_number
 #' @importFrom matrixStats colCumsums colDiffs colMins colMaxs rowSds
 #' @importFrom Matrix rsparsematrix
-#' @importFrom feasts unitroot_nsdiffs feat_stl unitroot_ndiffs unitroot_kpss
+#' @importFrom feasts unitroot_ndiffs unitroot_kpss
 #' @importFrom rlang is_empty .data abort
 #' @importFrom stats complete.cases diffinv na.omit optim pacf qnorm runif
 #' @importFrom utils tail
-#' @importFrom FitAR SelectModel
 #' @import Rcpp
 #' @import RcppArmadillo
 #' @import tsibble
