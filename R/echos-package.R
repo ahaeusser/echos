bb1a7d9bff18cc0c87ec37ee47e19a4a935264da
Alexander Haeusser <alexander-haeusser@gmx.de>
2025-03-04 14:21:22 +0100

    Update NAMESPACE and package website

--- R/echos-package.R
+++ R/echos-package.R
@@ -5,7 +5,7 @@
 #' @importFrom forecast ndiffs
 #' @importFrom rlang is_empty .data abort
 #' @importFrom stats complete.cases diffinv na.omit runif
-#' @importFrom graphics lines par
+#' @importFrom graphics lines par abline
 #' @importFrom utils tail
 #' @importFrom distributional dist_normal
 #' @import Rcpp
