07a0dabf3ff9733aaa993de58fb2bb8f2e103ff3
Alexander Haeusser <alexander-haeusser@gmx.de>
2025-05-18 12:43:02 +0200

    Improved documentation

--- R/data.R
+++ R/data.R
@@ -2,7 +2,15 @@
 #' @title M4 dataset
 #'
 #' @description \code{tsibble} with six monthly time series from the M4 
-#'   Forecasting Competition.
+#'   Forecasting Competition. The datasets contains the following time series:
+#'   \itemize{
+#'      \item{M21655 (Demographic), 1995 Jan - 2015 Mar}
+#'      \item{M21683 (Demographic), 2000 Jan - 2023 Apr}
+#'      \item{M2717 (Macro), 1996 Jan - 2016 Nov}
+#'      \item{M28597 (Industry), 1996 Jan - 2016 Dec}
+#'      \item{M42529 (Finance), 2001 Jan - 2009 Apr}
+#'      \item{M4813 (Macro), 1994 Apr - 2006 May}
+#'   }
 #'
 #' @docType data
 #'
@@ -26,10 +34,20 @@
 
 #' @title Synthetic data
 #'
-#' @description \code{tibble} with ten synthetic time series, i.e., Square Wave,
-#'   Sawtooth Wave, Harmonic Wave, Harmonic Wave w/ Trend, Amplitude Modulated
-#'   Wave, Frequency Modulated Wave, AR(1) Process, MA(2) Process, White Noise
-#'   Process and Random Walk Process
+#' @description \code{tibble} with ten synthetic time series. The dataset 
+#'   contains the following time series:
+#'   \itemize{
+#'      \item{Square Wave}
+#'      \item{Sawtooth Wave}
+#'      \item{Harmonic Wave}
+#'      \item{Harmonic Wave w/ Trend}
+#'      \item{Amplitude Modulated Wave}
+#'      \item{Frequency Modulated Wave}
+#'      \item{AR(1) Process}
+#'      \item{MA(2) Process}
+#'      \item{White Noise Process}
+#'      \item{Random Walk Process}
+#'   }
 #'
 #' @docType data
 #'
@@ -38,7 +56,7 @@
 #' @format An object of class \code{tibble} with 2.000 rows and 3 columns:
 #'    \itemize{
 #'       \item{\code{variable}: Unique identifier as \code{character} (key variable).}
-#'       \item{\code{index}: Index as \code{inteher} (index variable).}
+#'       \item{\code{index}: Index as \code{integer} (index variable).}
 #'       \item{\code{value}: Value as \code{numeric} (measurement variable).}
 #'       }
 #'
