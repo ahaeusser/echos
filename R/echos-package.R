a63656f8f64156d00b788827cc4345365a84df18
Alexander Haeusser <alexander-haeusser@gmx.de>
2025-06-02 11:07:29 +0200

    Adjusted dependencies

--- R/echos-package.R
+++ R/echos-package.R
@@ -2,14 +2,13 @@
 #' @importFrom purrr map map_dfr
 #' @importFrom tidyr pivot_longer
 #' @importFrom Matrix rsparsematrix
-#' @importFrom matrixStats rowQuantiles
 #' @importFrom forecast ndiffs
 #' @importFrom rlang is_empty .data abort
-#' @importFrom stats complete.cases diffinv na.omit runif
+#' @importFrom stats complete.cases diffinv na.omit runif quantile sd
 #' @importFrom graphics lines par abline matplot polygon
 #' @importFrom grDevices adjustcolor colorRampPalette
 #' @importFrom utils tail
-#' @importFrom distributional dist_sample
+#' @importFrom distributional dist_normal dist_sample
 #' @import Rcpp
 #' @import RcppArmadillo
 #' @import tsibble
