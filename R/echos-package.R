322d590e892be6c04bca76fe1c326f08e48feccf
Alexander Haeusser <alexander-haeusser@gmx.de>
2026-01-25 15:52:15 +0100

    Update imports and exports

--- R/echos-package.R
+++ R/echos-package.R
@@ -1,12 +1,12 @@
 
-#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_longer expand_grid
 #' @importFrom rlang .data enquo sym
 #' @importFrom stats complete.cases diffinv na.omit runif quantile sd lm residuals
 #' @importFrom graphics lines par abline matplot polygon
 #' @importFrom grDevices adjustcolor colorRampPalette
 #' @importFrom utils tail
 #' @importFrom distributional dist_normal dist_sample
-#' @importFrom dplyr select filter mutate arrange bind_rows bind_cols
+#' @importFrom dplyr select filter mutate arrange bind_rows bind_cols left_join
 #' @import Rcpp
 #' @import RcppArmadillo
 #' @import tsibble
